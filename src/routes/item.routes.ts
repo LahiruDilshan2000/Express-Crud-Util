ed30cae87bc870782a8fffdd6d2cf4c20cf83587
LahiruDilshan2000 <UrLahiru2000@gmail.com>
2024-02-06 16:16:02 +0530

    add middle ware

--- src/routes/item.routes.ts
+++ src/routes/item.routes.ts
@@ -5,5 +5,9 @@ import * as ItemController from "../controller/item.controller"
 const router = express.Router();
 
 router.post('/', ItemController.saveItem);
+router.get('/', ItemController.getItem);
+router.get('/getAll', ItemController.getAllItems);
+router.put('/', ItemController.updateItem);
+router.delete('/', ItemController.deleteItem);
 
 export default router;
